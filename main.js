9d45828c8e84c1d8a48fa6bb4835923647359648
priya12-09 <73743030+priya12-09@users.noreply.github.com>
2020-12-24 20:34:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -43,9 +43,40 @@ function my_keydown(e){
         console.log("right");
     }
 }
+function up() {
+    if (rover_y>=0) {
+        rover_y=rover_y-10;
+        console.log("when up arrow is pressed x="+rover_x+"y="+rover_y );
+        uploadBackground();
+        uploadrover();
+    }
+}
 
+function down() {
+    if (rover_y<=500) {
+        rover_y=rover_y+10;
+        console.log("when up arrow is pressed x="+rover_x+"y="+rover_y );
+        uploadBackground();
+        uploadrover();
+    }
+}
 
+function left() {
+    if (rover_x>=0) {
+        rover_x=rover_x-10;
+        console.log("when up arrow is pressed x="+rover_x+"y="+rover_y );
+        uploadBackground();
+        uploadrover();
+    }
+}
 
-
+function right() {
+    if (rover_x<=700) {
+        rover_x=rover_x+10;
+        console.log("when up arrow is pressed x="+rover_x+"y="+rover_y );
+        uploadBackground();
+        uploadrover();
+    }
+}
 
 
